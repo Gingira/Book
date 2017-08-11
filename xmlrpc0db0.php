b8f19ba57afa8e0c5760a580356618ae019749e1
Gingira <Shiffgtx@gmail.com>
2017-08-11 18:44:32 +0200

    Book
    Book

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
   <service>
     <engineName>WordPress</engineName>
     <engineLink>https://wordpress.org/</engineLink>
-    <homePageLink>http://127.0.0.1</homePageLink>
+    <homePageLink>http://localhost</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="http://127.0.0.1/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://127.0.0.1/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://127.0.0.1/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="http://127.0.0.1/xmlrpc.php" />
-      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://127.0.0.1/wp-json/" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://localhost/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://localhost/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://localhost/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://localhost/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://localhost/wp-json/" />
 	    </apis>
   </service>
 </rsd>
